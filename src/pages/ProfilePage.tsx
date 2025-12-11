7adcc88bf89268b3615ce029e5be40c7bfe58c41
Khai Truong <khai.truong@codelink.io>
2025-12-12 03:32:13 +0700

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -59,7 +59,7 @@ const ProfilePage = () => {
 		return (
 			<div className="flex flex-col items-center min-h-[70vh] w-full pt-10">
 				<h2 className="text-2xl font-bold text-gray-800 mb-4">2025年まとめ</h2>
-				<p className="text-gray-600">本を読まぬ者、まとめられず</p>
+				<p className="text-gray-600">本を読まぬ者、まとめられぬ</p>
 			</div>
 		);
 	}
